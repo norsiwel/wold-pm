abc0651c95b484151fffa89eb018ff51032fb83a
Norsiwel <ronel493@gmail.com>
2026-05-26 16:12:22 -0500

    feat: add Folk Interlude show — Lead Belly, Woody Guthrie, Cisco Houston

---
+++ shows/FolkInterlude/show.js
@@ -0,0 +1,10 @@
+const folkInterlude = [
+  { title: "~ Folk Interlude ~", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/bits/folk_intro.mp3" },
+  { title: "01 - MIDNIGHT SPECIAL - LEAD BELLY - Woody Guthrie", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/shows/FolkInterlude/01 - MIDNIGHT SPECIAL - LEAD BELLY - Woody Guthrie.mp3" },
+  { title: "02 - HAM & EGGS - LEAD BELLY - Woody Guthrie - Cisco Houston", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/shows/FolkInterlude/02 - HAM & EGGS - LEAD BELLY - Woody Guthrie - Cisco Houston.mp3" },
+  { title: "03 - GREY GOOSE - LEAD BELLY - Woody Guthrie - Cisco Houston", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/shows/FolkInterlude/03 - GREY GOOSE - LEAD BELLY - Woody Guthrie - Cisco Houston.mp3" },
+  { title: "04 - YELLOW GAL - LEAD BELLY - Woody Guthrie - Cisco Houston", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/shows/FolkInterlude/04 - YELLOW GAL - LEAD BELLY - Woody Guthrie - Cisco Houston.mp3" },
+  { title: "05 - ALABAMA BOUND - LEAD BELLY - Woody Guthrie", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/shows/FolkInterlude/05 - ALABAMA BOUND - LEAD BELLY - Woody Guthrie.mp3" },
+  { title: "06 - STEWBALL - LEAD BELLY - Woody Guthrie - Cisco Houston", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/shows/FolkInterlude/06 - STEWBALL - LEAD BELLY - Woody Guthrie - Cisco Houston.mp3" },
+  { title: "~ Onward Through the Fog ~", file: "https://pub-70b842d65a4f4ada82dec98f8d446fa2.r2.dev/bits/folk_outro.mp3" },
+];
